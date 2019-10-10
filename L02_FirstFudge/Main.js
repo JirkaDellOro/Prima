ed45582b676c5f0dfc71fa89f6a7267fd1187db0
Jirka Dell'Oro-Friedl <del@hs-furtwangen.de>
2019-10-10 21:02:43 +0200

    L01_done

--- L02_FirstFudge/Main.js
+++ L02_FirstFudge/Main.js
@@ -5,9 +5,21 @@ var L02_FirstFudge;
     window.addEventListener("load", hndLoad);
     function hndLoad(_event) {
         const canvas = document.querySelector("canvas");
-        console.log(canvas);
-        let viewport = new ƒ.Viewport();
-        viewport.initialize("Viewport", null, null, canvas);
+        ƒ.RenderManager.initialize();
+        L02_FirstFudge.viewport = new ƒ.Viewport();
+        let mesh = new ƒ.MeshQuad();
+        let mtrSolidWhite = new ƒ.Material("SolidWhite", ƒ.ShaderUniColor, new ƒ.CoatColored(ƒ.Color.WHITE));
+        let cmpMesh = new ƒ.ComponentMesh(mesh);
+        let cmpMaterial = new ƒ.ComponentMaterial(mtrSolidWhite);
+        let node = new ƒ.Node("Quad");
+        node.addComponent(cmpMesh);
+        node.addComponent(cmpMaterial);
+        let camera = new ƒ.ComponentCamera();
+        camera.pivot.translate(new ƒ.Vector3(0, 0, 2));
+        L02_FirstFudge.viewport.initialize("Viewport", node, camera, canvas);
+        // ƒ.RenderManager.update();
+        L02_FirstFudge.viewport.draw();
+        ƒ.Debug.log(L02_FirstFudge.viewport);
     }
 })(L02_FirstFudge || (L02_FirstFudge = {}));
 //# sourceMappingURL=Main.js.map
--- L02_FirstFudge/Main.ts
+++ L02_FirstFudge/Main.ts
@@ -1,14 +1,32 @@
 namespace L02_FirstFudge {
     import ƒ = FudgeCore;
-    
+
     window.addEventListener("load", hndLoad);
-    
+    export let viewport: ƒ.Viewport;
+
     function hndLoad(_event: Event): void {
         const canvas: HTMLCanvasElement = document.querySelector("canvas");
-        console.log(canvas);
-        
-        let viewport: ƒ.Viewport = new ƒ.Viewport();
-        viewport.initialize("Viewport", null, null,canvas);
+        ƒ.RenderManager.initialize();
+        viewport = new ƒ.Viewport();
 
+        let mesh: ƒ.MeshQuad = new ƒ.MeshQuad();
+        let mtrSolidWhite: ƒ.Material = new ƒ.Material("SolidWhite", ƒ.ShaderUniColor, new ƒ.CoatColored(ƒ.Color.WHITE));
+
+        let cmpMesh: ƒ.ComponentMesh = new ƒ.ComponentMesh(mesh);
+        let cmpMaterial: ƒ.ComponentMaterial = new ƒ.ComponentMaterial(mtrSolidWhite);
+        let node: ƒ.Node = new ƒ.Node("Quad");
+        node.addComponent(cmpMesh);
+        node.addComponent(cmpMaterial);
+
+
+        let camera: ƒ.ComponentCamera = new ƒ.ComponentCamera();
+        camera.pivot.translate(new ƒ.Vector3(0, 0, 2));
+
+        viewport.initialize("Viewport", node, camera, canvas);
+        
+        // ƒ.RenderManager.update();
+        viewport.draw();
+
+        ƒ.Debug.log(viewport);
     }
 }
