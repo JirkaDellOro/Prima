5dfd91305f68bcc697ac8e3f1f993ca4a616dc23
Jirka Dell'Oro-Friedl <del@hs-furtwangen.de>
2021-03-18 16:18:23 +0100

    L01 halfways

---
+++ L01_FirstFudge/Test.js
@@ -0,0 +1,8 @@
+"use strict";
+var L01_FirstFudge;
+(function (L01_FirstFudge) {
+    var ƒ = FudgeCore;
+    let node = new ƒ.Node("Test");
+    console.log(node);
+})(L01_FirstFudge || (L01_FirstFudge = {}));
+//# sourceMappingURL=Test.js.map
---
+++ L01_FirstFudge/Test.ts
@@ -0,0 +1,6 @@
+namespace L01_FirstFudge {
+  import ƒ = FudgeCore;
+
+  let node: ƒ.Node = new ƒ.Node("Test");
+  console.log(node);
+}
