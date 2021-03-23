408d7de48f273035012b4def08cb81d81b099da1
Jirka Dell'Oro-Friedl <del@hs-furtwangen.de>
2021-03-23 18:17:26 +0100

    Furst Fudge done

--- L01_FirstFudge/Test.js
+++ L01_FirstFudge/Test.js
@@ -2,7 +2,22 @@
 var L01_FirstFudge;
 (function (L01_FirstFudge) {
     var ƒ = FudgeCore;
-    let node = new ƒ.Node("Test");
-    console.log(node);
+    window.addEventListener("load", init);
+    function init(_event) {
+        let node = new ƒ.Node("Test");
+        const canvas = document.querySelector("canvas");
+        let mesh = new ƒ.MeshQuad("Quad");
+        node.addComponent(new ƒ.ComponentMesh(mesh));
+        let material = new ƒ.Material("Florian", ƒ.ShaderUniColor, new ƒ.CoatColored(new ƒ.Color(1, 1, 1, 1)));
+        let cmpMaterial = new ƒ.ComponentMaterial(material);
+        node.addComponent(cmpMaterial);
+        let cmpCamera = new ƒ.ComponentCamera();
+        cmpCamera.mtxPivot.translateZ(3);
+        cmpCamera.mtxPivot.rotateY(180);
+        console.log(cmpCamera);
+        let viewport = new ƒ.Viewport();
+        viewport.initialize("Viewport", node, cmpCamera, canvas);
+        viewport.draw();
+    }
 })(L01_FirstFudge || (L01_FirstFudge = {}));
 //# sourceMappingURL=Test.js.map
--- L01_FirstFudge/Test.ts
+++ L01_FirstFudge/Test.ts
@@ -1,6 +1,25 @@
 namespace L01_FirstFudge {
   import ƒ = FudgeCore;
+  window.addEventListener("load", init);
 
-  let node: ƒ.Node = new ƒ.Node("Test");
-  console.log(node);
+  function init(_event: Event): void {
+    let node: ƒ.Node = new ƒ.Node("Test");
+    const canvas: HTMLCanvasElement = document.querySelector("canvas");
+
+    let mesh: ƒ.MeshQuad = new ƒ.MeshQuad("Quad");
+    node.addComponent(new ƒ.ComponentMesh(mesh));
+    
+    let material: ƒ.Material = new ƒ.Material("Florian", ƒ.ShaderUniColor, new ƒ.CoatColored(new ƒ.Color(1, 1, 1, 1)));
+    let cmpMaterial: ƒ.ComponentMaterial = new ƒ.ComponentMaterial(material);
+    node.addComponent(cmpMaterial);
+    
+    let cmpCamera: ƒ.ComponentCamera = new ƒ.ComponentCamera();
+    cmpCamera.mtxPivot.translateZ(3);
+    cmpCamera.mtxPivot.rotateY(180);
+    console.log(cmpCamera);
+
+    let viewport: ƒ.Viewport = new ƒ.Viewport();
+    viewport.initialize("Viewport", node, cmpCamera, canvas);
+    viewport.draw();
+  }
 }
