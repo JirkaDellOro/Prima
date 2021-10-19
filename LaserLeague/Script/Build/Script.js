eb265ea5a8964c07baf81743f8d19bc69a58f0dd
Jirka Dell'Oro-Friedl <del@hs-furtwangen.de>
2021-10-19 15:11:48 +0200

    wip controls

--- LaserLeague/Script/Build/Script.js
+++ LaserLeague/Script/Build/Script.js
@@ -39,16 +39,22 @@ var Script;
     let viewport;
     document.addEventListener("interactiveViewportStarted", start);
     let transform;
+    let agent;
     function start(_event) {
         viewport = _event.detail;
-        ƒ.Loop.addEventListener("loopFrame" /* LOOP_FRAME */, update);
-        ƒ.Loop.start(); // start the game loop to continously draw the viewport, update the audiosystem and drive the physics i/a
         let graph = viewport.getBranch();
         let laser = graph.getChildrenByName("Lasers")[0].getChildrenByName("Laser")[0];
         transform = laser.getComponent(ƒ.ComponentTransform).mtxLocal;
+        agent = graph.getChildrenByName("Agents")[0].getChildren()[0];
+        ƒ.Loop.addEventListener("loopFrame" /* LOOP_FRAME */, update);
+        ƒ.Loop.start(); // start the game loop to continously draw the viewport, update the audiosystem and drive the physics i/a
     }
     function update(_event) {
         // ƒ.Physics.world.simulate();  // if physics is included and used
+        if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.W, ƒ.KEYBOARD_CODE.ARROW_UP]))
+            agent.mtxLocal.translateY(0.1);
+        if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.S, ƒ.KEYBOARD_CODE.ARROW_DOWN]))
+            agent.mtxLocal.translateY(-0.1);
         transform.rotateZ(5);
         viewport.draw();
         ƒ.AudioManager.default.update();
--- LaserLeague/Script/Source/Main.ts
+++ LaserLeague/Script/Source/Main.ts
@@ -6,20 +6,27 @@ namespace Script {
   document.addEventListener("interactiveViewportStarted", <EventListener>start);
 
   let transform: ƒ.Matrix4x4;
+  let agent: ƒ.Node;
 
   function start(_event: CustomEvent): void {
     viewport = _event.detail;
 
-    ƒ.Loop.addEventListener(ƒ.EVENT.LOOP_FRAME, update);
-    ƒ.Loop.start();  // start the game loop to continously draw the viewport, update the audiosystem and drive the physics i/a
-
     let graph: ƒ.Node = viewport.getBranch();
     let laser: ƒ.Node = graph.getChildrenByName("Lasers")[0].getChildrenByName("Laser")[0];
     transform = laser.getComponent(ƒ.ComponentTransform).mtxLocal;
+    agent = graph.getChildrenByName("Agents")[0].getChildren()[0];
+
+    ƒ.Loop.addEventListener(ƒ.EVENT.LOOP_FRAME, update);
+    ƒ.Loop.start();  // start the game loop to continously draw the viewport, update the audiosystem and drive the physics i/a
   }
 
   function update(_event: Event): void {
     // ƒ.Physics.world.simulate();  // if physics is included and used
+    if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.W, ƒ.KEYBOARD_CODE.ARROW_UP]))
+      agent.mtxLocal.translateY(0.1);
+    if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.S, ƒ.KEYBOARD_CODE.ARROW_DOWN]))
+      agent.mtxLocal.translateY(-0.1);
+
     transform.rotateZ(5);
     viewport.draw();
     ƒ.AudioManager.default.update();
