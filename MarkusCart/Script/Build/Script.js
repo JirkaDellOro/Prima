5d248fae98f90331d0d420063f74bea6c6ad184a
Jirka Dell'Oro-Friedl <del@hs-furtwangen.de>
2021-11-23 15:10:04 +0100

    controls

--- MarkusCart/Script/Build/Script.js
+++ MarkusCart/Script/Build/Script.js
@@ -1,1 +1,74 @@
-console.warn("%cFor scripting, start your favorite IDE (e.g. VSCode) with typescript installed, load and compile this project", "color: red; font-weight: bold;");
+"use strict";
+var Script;
+(function (Script) {
+    var ƒ = FudgeCore;
+    ƒ.Project.registerScriptNamespace(Script); // Register the namespace to FUDGE for serialization
+    class CustomComponentScript extends ƒ.ComponentScript {
+        // Register the script as component for use in the editor via drag&drop
+        static iSubclass = ƒ.Component.registerSubclass(CustomComponentScript);
+        // Properties may be mutated by users in the editor via the automatically created user interface
+        message = "CustomComponentScript added to ";
+        constructor() {
+            super();
+            // Don't start when running in editor
+            if (ƒ.Project.mode == ƒ.MODE.EDITOR)
+                return;
+            // Listen to this component being added to or removed from a node
+            this.addEventListener("componentAdd" /* COMPONENT_ADD */, this.hndEvent);
+            this.addEventListener("componentRemove" /* COMPONENT_REMOVE */, this.hndEvent);
+        }
+        // Activate the functions of this component as response to events
+        hndEvent = (_event) => {
+            switch (_event.type) {
+                case "componentAdd" /* COMPONENT_ADD */:
+                    ƒ.Debug.log(this.message, this.node);
+                    break;
+                case "componentRemove" /* COMPONENT_REMOVE */:
+                    this.removeEventListener("componentAdd" /* COMPONENT_ADD */, this.hndEvent);
+                    this.removeEventListener("componentRemove" /* COMPONENT_REMOVE */, this.hndEvent);
+                    break;
+            }
+        };
+    }
+    Script.CustomComponentScript = CustomComponentScript;
+})(Script || (Script = {}));
+var Script;
+(function (Script) {
+    var ƒ = FudgeCore;
+    ƒ.Debug.info("Main Program Template running!");
+    let viewport;
+    let cart;
+    // let mtxTerrain: ƒ.Matrix4x4;
+    // let meshTerrain: ƒ.MeshTerrain;
+    let ctrForward = new ƒ.Control("Forward", 10, 0 /* PROPORTIONAL */);
+    ctrForward.setDelay(200);
+    let ctrTurn = new ƒ.Control("Forward", 100, 0 /* PROPORTIONAL */);
+    ctrForward.setDelay(50);
+    document.addEventListener("interactiveViewportStarted", start);
+    function start(_event) {
+        viewport = _event.detail;
+        viewport.calculateTransforms();
+        // let cmpMeshTerrain: ƒ.ComponentMesh = viewport.getBranch().getChildrenByName("Terrain")[0].getComponent(ƒ.ComponentMesh);
+        // meshTerrain = <ƒ.MeshTerrain>cmpMeshTerrain.mesh;
+        // mtxTerrain = cmpMeshTerrain.mtxWorld;
+        cart = viewport.getBranch().getChildrenByName("Cart")[0];
+        ƒ.Loop.addEventListener("loopFrame" /* LOOP_FRAME */, update);
+        ƒ.Loop.start(); // start the game loop to continously draw the viewport, update the audiosystem and drive the physics i/a
+    }
+    function update(_event) {
+        // ƒ.Physics.world.simulate();  // if physics is included and used
+        let deltaTime = ƒ.Loop.timeFrameReal / 1000;
+        let turn = ƒ.Keyboard.mapToTrit([ƒ.KEYBOARD_CODE.A, ƒ.KEYBOARD_CODE.ARROW_LEFT], [ƒ.KEYBOARD_CODE.D, ƒ.KEYBOARD_CODE.ARROW_RIGHT]);
+        ctrTurn.setInput(turn * deltaTime);
+        cart.mtxLocal.rotateY(ctrTurn.getOutput());
+        let forward = ƒ.Keyboard.mapToTrit([ƒ.KEYBOARD_CODE.W, ƒ.KEYBOARD_CODE.ARROW_UP], [ƒ.KEYBOARD_CODE.S, ƒ.KEYBOARD_CODE.ARROW_DOWN]);
+        ctrForward.setInput(forward * deltaTime);
+        cart.mtxLocal.translateZ(ctrForward.getOutput());
+        // let terrainInfo: ƒ.TerrainInfo = meshTerrain.getTerrainInfo(cart.mtxLocal.translation, mtxTerrain);
+        // cart.mtxLocal.translation = terrainInfo.position;
+        // cart.mtxLocal.showTo(ƒ.Vector3.SUM(terrainInfo.position, cart.mtxLocal.getZ()), terrainInfo.normal);
+        viewport.draw();
+        ƒ.AudioManager.default.update();
+    }
+})(Script || (Script = {}));
+//# sourceMappingURL=Script.js.map
--- MarkusCart/Script/Source/Main.ts
+++ MarkusCart/Script/Source/Main.ts
@@ -3,17 +3,46 @@ namespace Script {
   ƒ.Debug.info("Main Program Template running!");
 
   let viewport: ƒ.Viewport;
+  let cart: ƒ.Node;
+  // let mtxTerrain: ƒ.Matrix4x4;
+  // let meshTerrain: ƒ.MeshTerrain;
+
+  let ctrForward: ƒ.Control = new ƒ.Control("Forward", 10, ƒ.CONTROL_TYPE.PROPORTIONAL);
+  ctrForward.setDelay(200);
+  let ctrTurn: ƒ.Control = new ƒ.Control("Forward", 100, ƒ.CONTROL_TYPE.PROPORTIONAL);
+  ctrForward.setDelay(50);
+
   document.addEventListener("interactiveViewportStarted", <EventListener>start);
 
   function start(_event: CustomEvent): void {
     viewport = _event.detail;
+    viewport.calculateTransforms();
+
+    // let cmpMeshTerrain: ƒ.ComponentMesh = viewport.getBranch().getChildrenByName("Terrain")[0].getComponent(ƒ.ComponentMesh);
+    // meshTerrain = <ƒ.MeshTerrain>cmpMeshTerrain.mesh;
+    // mtxTerrain = cmpMeshTerrain.mtxWorld;
+    cart = viewport.getBranch().getChildrenByName("Cart")[0];
 
     ƒ.Loop.addEventListener(ƒ.EVENT.LOOP_FRAME, update);
-    // ƒ.Loop.start();  // start the game loop to continously draw the viewport, update the audiosystem and drive the physics i/a
+    ƒ.Loop.start();  // start the game loop to continously draw the viewport, update the audiosystem and drive the physics i/a
   }
 
   function update(_event: Event): void {
     // ƒ.Physics.world.simulate();  // if physics is included and used
+    let deltaTime: number = ƒ.Loop.timeFrameReal / 1000;
+
+    let turn: number = ƒ.Keyboard.mapToTrit([ƒ.KEYBOARD_CODE.A, ƒ.KEYBOARD_CODE.ARROW_LEFT], [ƒ.KEYBOARD_CODE.D, ƒ.KEYBOARD_CODE.ARROW_RIGHT]);
+    ctrTurn.setInput(turn * deltaTime);
+    cart.mtxLocal.rotateY(ctrTurn.getOutput());
+
+    let forward: number = ƒ.Keyboard.mapToTrit([ƒ.KEYBOARD_CODE.W, ƒ.KEYBOARD_CODE.ARROW_UP], [ƒ.KEYBOARD_CODE.S, ƒ.KEYBOARD_CODE.ARROW_DOWN]);
+    ctrForward.setInput(forward * deltaTime);
+    cart.mtxLocal.translateZ(ctrForward.getOutput());
+
+    // let terrainInfo: ƒ.TerrainInfo = meshTerrain.getTerrainInfo(cart.mtxLocal.translation, mtxTerrain);
+    // cart.mtxLocal.translation = terrainInfo.position;
+    // cart.mtxLocal.showTo(ƒ.Vector3.SUM(terrainInfo.position, cart.mtxLocal.getZ()), terrainInfo.normal);
+
     viewport.draw();
     ƒ.AudioManager.default.update();
   }
