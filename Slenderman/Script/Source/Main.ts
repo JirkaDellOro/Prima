a9c64c0efd3b8edec927a30c3c14565afae660cf
Jirka Dell'Oro-Friedl <del@hs-furtwangen.de>
2022-04-15 16:53:16 +0200

    tinker walk

--- Slenderman/Script/Source/Main.ts
+++ Slenderman/Script/Source/Main.ts
@@ -3,18 +3,37 @@ namespace Script {
   ƒ.Debug.info("Main Program Template running!");
 
   let viewport: ƒ.Viewport;
+  let avatar: ƒ.Node;
+  let speedRotY: number = 0.5;
+  let cntWalk: ƒ.Control = new ƒ.Control("ControlWalk", 6, ƒ.CONTROL_TYPE.PROPORTIONAL);
+
+
   document.addEventListener("interactiveViewportStarted", <EventListener>start);
 
   function start(_event: CustomEvent): void {
     viewport = _event.detail;
+    avatar = viewport.getBranch().getChildrenByName("Avatar")[0];
+    viewport.camera = avatar.getChildrenByName("Camera")[0].getComponent(ƒ.ComponentCamera);
+
+    document.addEventListener("pointermove", hndPointer);
 
     ƒ.Loop.addEventListener(ƒ.EVENT.LOOP_FRAME, update);
-    // ƒ.Loop.start();  // start the game loop to continously draw the viewport, update the audiosystem and drive the physics i/a
+    ƒ.Loop.start();  // start the game loop to continously draw the viewport, update the audiosystem and drive the physics i/a
   }
 
   function update(_event: Event): void {
     // ƒ.Physics.simulate();  // if physics is included and used
+    control();
     viewport.draw();
     ƒ.AudioManager.default.update();
+  }
+
+  function control(): void {
+    let input: number = ƒ.Keyboard.mapToTrit([ƒ.KEYBOARD_CODE.W, ƒ.KEYBOARD_CODE.ARROW_UP], [ƒ.KEYBOARD_CODE.S, ƒ.KEYBOARD_CODE.ARROW_DOWN]);
+    cntWalk.setInput(input);
+    avatar.mtxLocal.translateZ(cntWalk.getOutput() * ƒ.Loop.timeFrameGame / 1000);
+  }
+  function hndPointer(_event: PointerEvent): void {
+    avatar.mtxLocal.rotateY(speedRotY * _event.movementX);
   }
 }
