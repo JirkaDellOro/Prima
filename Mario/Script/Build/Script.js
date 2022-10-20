4111efaed93632231eeda6e302860321eb4ebafe
Jirka Dell'Oro-Friedl <del@hs-furtwangen.de>
2022-10-20 15:29:41 +0200

    Henry's Mario jumps

--- Mario/Script/Build/Script.js
+++ Mario/Script/Build/Script.js
@@ -39,18 +39,118 @@ var Script;
 var Script;
 (function (Script) {
     var ƒ = FudgeCore;
-    ƒ.Debug.info("Main Program Template running!");
+    var ƒAid = FudgeAid;
+    // Initialize Viewport
     let viewport;
     document.addEventListener("interactiveViewportStarted", start);
     function start(_event) {
         viewport = _event.detail;
-        ƒ.Loop.addEventListener("loopFrame" /* LOOP_FRAME */, update);
-        // ƒ.Loop.start();  // start the game loop to continously draw the viewport, update the audiosystem and drive the physics i/a
+        hndLoad(_event);
     }
+    let animWalk;
+    let animSprint;
+    let animJump;
+    let animLook;
+    let animDeath;
+    function initializeAnimations(coat) {
+        animWalk = new ƒAid.SpriteSheetAnimation("Walk", coat);
+        animWalk.generateByGrid(ƒ.Rectangle.GET(0, 0, 16, 24), 2, 64, ƒ.ORIGIN2D.BOTTOMCENTER, ƒ.Vector2.X(16));
+        animSprint = new ƒAid.SpriteSheetAnimation("Sprint", coat);
+        animSprint.generateByGrid(ƒ.Rectangle.GET(0, 24, 16, 24), 2, 64, ƒ.ORIGIN2D.BOTTOMCENTER, ƒ.Vector2.X(16));
+        animJump = new ƒAid.SpriteSheetAnimation("Jump", coat);
+        animJump.generateByGrid(ƒ.Rectangle.GET(0, 48, 16, 24), 2, 64, ƒ.ORIGIN2D.BOTTOMCENTER, ƒ.Vector2.X(16));
+        animLook = new ƒAid.SpriteSheetAnimation("Look", coat);
+        animLook.generateByGrid(ƒ.Rectangle.GET(32, 0, 16, 24), 2, 64, ƒ.ORIGIN2D.BOTTOMCENTER, ƒ.Vector2.X(16));
+        animDeath = new ƒAid.SpriteSheetAnimation("Death", coat);
+        animDeath.generateByGrid(ƒ.Rectangle.GET(32, 24, 16, 24), 2, 64, ƒ.ORIGIN2D.BOTTOMCENTER, ƒ.Vector2.X(16));
+    }
+    // Load Sprite
+    let avatar;
+    async function hndLoad(_event) {
+        let imgSpriteSheet = new ƒ.TextureImage();
+        await imgSpriteSheet.load("./Images/Mario_Spritesheet.png");
+        let coat = new ƒ.CoatTextured(undefined, imgSpriteSheet);
+        initializeAnimations(coat);
+        avatar = new ƒAid.NodeSprite("Avatar");
+        avatar.addComponent(new ƒ.ComponentTransform(new ƒ.Matrix4x4()));
+        avatar.setAnimation(animWalk);
+        avatar.framerate = 20;
+        avatar.mtxLocal.translateY(0.3);
+        avatar.mtxLocal.translateX(-1);
+        avatar.mtxLocal.translateZ(1.001);
+        let branch = viewport.getBranch();
+        // let mario: ƒ.Node = branch.getChildrenByName("Mario")[0];
+        branch.addChild(avatar);
+        ƒ.Loop.addEventListener("loopFrame" /* LOOP_FRAME */, update);
+        ƒ.Loop.start();
+    }
+    const xSpeedDefault = .9;
+    const xSpeedSprint = 2;
+    let ySpeed = 0.01;
+    let gravity = 0.05;
+    let leftDirection = false;
+    let prevSprint = false;
     function update(_event) {
-        // ƒ.Physics.simulate();  // if physics is included and used
+        let deltaTime = ƒ.Loop.timeFrameGame / 1000;
+        ySpeed -= gravity * deltaTime;
+        avatar.mtxLocal.translateY(ySpeed);
+        if (avatar.mtxLocal.translation.y < 0)
+            avatar.mtxLocal.translation.y = 0;
+        let speed = xSpeedDefault;
+        if (leftDirection) {
+            speed = -xSpeedDefault;
+        }
+        if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.SHIFT_LEFT, ƒ.KEYBOARD_CODE.SHIFT_RIGHT])) {
+            speed = xSpeedSprint;
+            if (leftDirection) {
+                speed = -xSpeedSprint;
+            }
+        }
+        // Calculate (walk) speed
+        const xTranslation = speed * deltaTime;
+        // Check for key presses
+        if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.A, ƒ.KEYBOARD_CODE.ARROW_LEFT])) {
+            avatar.mtxLocal.translateX(-xTranslation);
+            leftDirection = true;
+            if (speed < -1) {
+                if (!prevSprint) {
+                    prevSprint = true;
+                    avatar.setAnimation(animSprint);
+                }
+            }
+            else {
+                prevSprint = false;
+            }
+        }
+        else if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.D, ƒ.KEYBOARD_CODE.ARROW_RIGHT])) {
+            avatar.mtxLocal.translateX(xTranslation);
+            leftDirection = false;
+            if (speed > 1) {
+                if (!prevSprint) {
+                    prevSprint = true;
+                    avatar.setAnimation(animSprint);
+                }
+            }
+            else {
+                prevSprint = false;
+            }
+        }
+        else if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.W, ƒ.KEYBOARD_CODE.ARROW_UP])) {
+            avatar.setAnimation(animLook);
+            avatar.showFrame(1);
+        }
+        else if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.S, ƒ.KEYBOARD_CODE.ARROW_DOWN])) {
+            avatar.setAnimation(animLook);
+            avatar.showFrame(0);
+        }
+        else {
+            avatar.showFrame(0);
+            avatar.setAnimation(animWalk);
+        }
+        // Rotate based on direction
+        avatar.mtxLocal.rotation = ƒ.Vector3.Y(leftDirection ? 180 : 0);
         viewport.draw();
-        ƒ.AudioManager.default.update();
+        //ƒ.AudioManager.default.update();
     }
 })(Script || (Script = {}));
 //# sourceMappingURL=Script.js.map
--- Mario/Script/Source/Main.ts
+++ Mario/Script/Source/Main.ts
@@ -1,20 +1,135 @@
 namespace Script {
   import ƒ = FudgeCore;
-  ƒ.Debug.info("Main Program Template running!");
+  import ƒAid = FudgeAid;
 
+  // Initialize Viewport
   let viewport: ƒ.Viewport;
   document.addEventListener("interactiveViewportStarted", <EventListener>start);
 
   function start(_event: CustomEvent): void {
     viewport = _event.detail;
-
-    ƒ.Loop.addEventListener(ƒ.EVENT.LOOP_FRAME, update);
-    // ƒ.Loop.start();  // start the game loop to continously draw the viewport, update the audiosystem and drive the physics i/a
+    hndLoad(_event);
   }
 
+  let animWalk: ƒAid.SpriteSheetAnimation;
+  let animSprint: ƒAid.SpriteSheetAnimation;
+  let animJump: ƒAid.SpriteSheetAnimation;
+  let animLook: ƒAid.SpriteSheetAnimation;
+  let animDeath: ƒAid.SpriteSheetAnimation;
+
+  function initializeAnimations(coat: ƒ.CoatTextured): void {
+    animWalk = new ƒAid.SpriteSheetAnimation("Walk", coat);
+    animWalk.generateByGrid(ƒ.Rectangle.GET(0, 0, 16, 24), 2, 64, ƒ.ORIGIN2D.BOTTOMCENTER, ƒ.Vector2.X(16));
+
+    animSprint = new ƒAid.SpriteSheetAnimation("Sprint", coat);
+    animSprint.generateByGrid(ƒ.Rectangle.GET(0, 24, 16, 24), 2, 64, ƒ.ORIGIN2D.BOTTOMCENTER, ƒ.Vector2.X(16));
+
+    animJump = new ƒAid.SpriteSheetAnimation("Jump", coat);
+    animJump.generateByGrid(ƒ.Rectangle.GET(0, 48, 16, 24), 2, 64, ƒ.ORIGIN2D.BOTTOMCENTER, ƒ.Vector2.X(16));
+
+    animLook = new ƒAid.SpriteSheetAnimation("Look", coat);
+    animLook.generateByGrid(ƒ.Rectangle.GET(32, 0, 16, 24), 2, 64, ƒ.ORIGIN2D.BOTTOMCENTER, ƒ.Vector2.X(16));
+
+    animDeath = new ƒAid.SpriteSheetAnimation("Death", coat);
+    animDeath.generateByGrid(ƒ.Rectangle.GET(32, 24, 16, 24), 2, 64, ƒ.ORIGIN2D.BOTTOMCENTER, ƒ.Vector2.X(16));
+  }
+
+  // Load Sprite
+  let avatar: ƒAid.NodeSprite;
+  async function hndLoad(_event: Event): Promise<void> {
+    let imgSpriteSheet: ƒ.TextureImage = new ƒ.TextureImage();
+    await imgSpriteSheet.load("./Images/Mario_Spritesheet.png");
+    let coat: ƒ.CoatTextured = new ƒ.CoatTextured(undefined, imgSpriteSheet);
+
+    initializeAnimations(coat);
+
+    avatar = new ƒAid.NodeSprite("Avatar");
+    avatar.addComponent(new ƒ.ComponentTransform(new ƒ.Matrix4x4()));
+    avatar.setAnimation(animWalk);
+    avatar.framerate = 20;
+
+    avatar.mtxLocal.translateY(0.3);
+    avatar.mtxLocal.translateX(-1);
+    avatar.mtxLocal.translateZ(1.001);
+
+    let branch: ƒ.Node = viewport.getBranch();
+    // let mario: ƒ.Node = branch.getChildrenByName("Mario")[0];
+    branch.addChild(avatar);
+
+    ƒ.Loop.addEventListener(ƒ.EVENT.LOOP_FRAME, update);
+    ƒ.Loop.start();
+  }
+
+  const xSpeedDefault: number = .9;
+  const xSpeedSprint: number = 2;
+  let ySpeed: number = 0.01;
+  let gravity: number = 0.05;
+
+  let leftDirection: boolean = false;
+  let prevSprint: boolean = false;
+
   function update(_event: Event): void {
-    // ƒ.Physics.simulate();  // if physics is included and used
+    let deltaTime: number = ƒ.Loop.timeFrameGame / 1000;
+    ySpeed -= gravity * deltaTime;
+    avatar.mtxLocal.translateY(ySpeed);
+
+    if (avatar.mtxLocal.translation.y < 0)
+      avatar.mtxLocal.translation.y = 0;
+
+    let speed: number = xSpeedDefault;
+    if (leftDirection) {
+      speed = -xSpeedDefault;
+    }
+
+    if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.SHIFT_LEFT, ƒ.KEYBOARD_CODE.SHIFT_RIGHT])) {
+      speed = xSpeedSprint;
+      if (leftDirection) {
+        speed = -xSpeedSprint;
+      }
+    }
+
+    // Calculate (walk) speed
+    const xTranslation: number = speed * deltaTime;
+
+    // Check for key presses
+    if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.A, ƒ.KEYBOARD_CODE.ARROW_LEFT])) {
+      avatar.mtxLocal.translateX(-xTranslation);
+      leftDirection = true;
+      if (speed < -1) {
+        if (!prevSprint) {
+          prevSprint = true;
+          avatar.setAnimation(animSprint);
+        }
+      } else {
+        prevSprint = false;
+      }
+    } else if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.D, ƒ.KEYBOARD_CODE.ARROW_RIGHT])) {
+      avatar.mtxLocal.translateX(xTranslation);
+      leftDirection = false;
+      if (speed > 1) {
+        if (!prevSprint) {
+          prevSprint = true;
+          avatar.setAnimation(animSprint);
+        }
+      } else {
+        prevSprint = false;
+      }
+    } else if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.W, ƒ.KEYBOARD_CODE.ARROW_UP])) {
+      avatar.setAnimation(animLook);
+      avatar.showFrame(1);
+    } else if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.S, ƒ.KEYBOARD_CODE.ARROW_DOWN])) {
+      avatar.setAnimation(animLook);
+      avatar.showFrame(0);
+    } else {
+      avatar.showFrame(0);
+      avatar.setAnimation(animWalk);
+    }
+
+    // Rotate based on direction
+    avatar.mtxLocal.rotation = ƒ.Vector3.Y(leftDirection ? 180 : 0);
+
+
     viewport.draw();
-    ƒ.AudioManager.default.update();
+    //ƒ.AudioManager.default.update();
   }
 }
