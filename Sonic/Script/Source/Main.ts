5185ac5dc0058982c6dbc3ce86b23ddc65f105c2
Jirka Dell'Oro-Friedl <del@hs-furtwangen.de>
2023-03-27 11:36:25 +0200

    Sonic moves

--- Sonic/Script/Source/Main.ts
+++ Sonic/Script/Source/Main.ts
@@ -3,17 +3,23 @@ namespace Script {
   ƒ.Debug.info("Main Program Template running!");
 
   let viewport: ƒ.Viewport;
+  let sonic: ƒ.Node;
   document.addEventListener("interactiveViewportStarted", <EventListener>start);
 
   function start(_event: CustomEvent): void {
     viewport = _event.detail;
 
+    sonic = viewport.getBranch().getChildrenByName("Sonic")[0];
+    console.log(sonic);
+
+
     ƒ.Loop.addEventListener(ƒ.EVENT.LOOP_FRAME, update);
-    // ƒ.Loop.start();  // start the game loop to continously draw the viewport, update the audiosystem and drive the physics i/a
+    ƒ.Loop.start();  // start the game loop to continously draw the viewport, update the audiosystem and drive the physics i/a
   }
 
   function update(_event: Event): void {
     // ƒ.Physics.simulate();  // if physics is included and used
+    sonic.mtxLocal.translateX(0.01);
     viewport.draw();
     ƒ.AudioManager.default.update();
   }
